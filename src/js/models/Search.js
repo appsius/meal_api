151577108cd326206cb769c3e9a3842cb16e32f9
appsius <ilyas.ustunkaya234434@gmail.com>
2020-04-17 15:24:26 +0300

    Building search model controller

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -10,7 +10,7 @@ export default class Search {
 			const res = await axios(
 				`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`
 			);
-			const result = res.data.recipes;
+			this.result = res.data.recipes;
 			// console.log(result);
 		} catch (error) {
 			alert(error);
