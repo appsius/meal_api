7735a581d90484557e9e77080a708bc09ca67beb
appsius <ilyas.ustunkaya234434@gmail.com>
2020-04-18 09:04:48 +0300

    Building the Recipe Controller

--- src/js/index.js
+++ src/js/index.js
@@ -28,12 +28,17 @@ const controlSearch = async () => {
 		searchView.clearResults();
 		renderLoader(elements.searchRes);
 
-		// 4) Search for recipes
-		await state.search.getResults();
+		try {
+			// 4) Search for recipes
+			await state.search.getResults();
 
-		// 5) Render results on UI
-		clearLoader();
-		searchView.renderResults(state.search.result);
+			// 5) Render results on UI
+			clearLoader();
+			searchView.renderResults(state.search.result);
+		} catch (err) {
+			alert('Something went wrong with search...');
+			clearLoader();
+		}
 	}
 };
 
@@ -55,6 +60,35 @@ elements.searchResPages.addEventListener('click', (e) => {
 //* RECIPE CONTROLLER
 //*********************
 
-const r = new Recipe(47746);
-r.getRecipe();
-console.log(r);
+const controlRecipe = async () => {
+	// Get id from url
+	const id = window.location.hash.replace('#', '');
+	console.log(id);
+
+	if (id) {
+		// Prepare UI for changes
+
+		// Create new recipe object
+		state.recipe = new Recipe(id);
+
+		try {
+			// Get recipe data
+			await state.recipe.getRecipe();
+
+			// Calculate servings and time
+			state.recipe.calcTime();
+			state.recipe.calcServings();
+
+			// Render recipe
+			console.log(state.recipe);
+		} catch (err) {
+			alert('Error processing recipe!');
+		}
+	}
+};
+
+// window.addEventListener('hashchange', controlRecipe);
+
+['hashchange', 'load'].forEach((event) =>
+	window.addEventListener(event, controlRecipe)
+);
--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -8,7 +8,7 @@ export default class Recipe {
 	async getRecipe() {
 		try {
 			const res = await axios(
-				`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`
+				`https://forkify-api.herokuapp.com/44444api/get?rId=${this.id}`
 			);
 			this.title = res.data.recipe.title;
 			this.author = res.data.recipe.publisher;
